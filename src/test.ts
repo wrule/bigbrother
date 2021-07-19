ddf855937a92558fd6659d874216a8f21da7d69b
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-19 14:00:02 +0800

    修改测试代码

--- src/test.ts
+++ src/test.ts
@@ -7,15 +7,15 @@ const priKey = fs.readFileSync('./keys/pri.key', 'utf8');
 const pubKey = fs.readFileSync('./keys/pub.key', 'utf8');
 
 BigBrother.Start(pubKey);
-const token1 = new Token({
-  prj: 'XSea',
-  ver: '*',
-  name: 'jimao',
-  type: 'axios',
-  addr: 'http://172.16.1.194:19841',
-}).Sign(priKey);
-console.log(token1);
-const watcher = BigBrother.NewWatcher(token1);
-setInterval(() => {
-  watcher('你好，世界');
-}, 2000);
+// const token1 = new Token({
+//   prj: 'XSea',
+//   ver: '*',
+//   name: 'jimao',
+//   type: 'axios',
+//   addr: 'http://172.16.1.194:19841',
+// }).Sign(priKey);
+// console.log(token1);
+// const watcher = BigBrother.NewWatcher(token1);
+// setInterval(() => {
+//   watcher('你好，世界');
+// }, 2000);
