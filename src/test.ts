71f2f2df73d8dd5f7b624987fca70209e67ec627
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-19 13:37:23 +0800

    测试代码

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,32 @@
 
 import * as BigBrother from './index';
+import { Token } from './token';
 
 console.log('你好，世界');
+
+const priKey = `
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIGtaBNn2rl7jHYO6GizijG8ASWd0pkutv6LA4RHjY97JoAcGBSuBBAAK
+oUQDQgAEMRZbsrnXprqJ8eeoat2Zedvej4EXufJVagH1fVkJeeKgw0DW6qQex7cK
+JtMJCsOmOK2xnbkEdLg2NopR1JdUmg==
+-----END EC PRIVATE KEY-----
+`;
+const pubKey = `
+-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEMRZbsrnXprqJ8eeoat2Zedvej4EXufJV
+agH1fVkJeeKgw0DW6qQex7cKJtMJCsOmOK2xnbkEdLg2NopR1JdUmg==
+-----END PUBLIC KEY-----
+`;
+
+BigBrother.Start(pubKey);
+const token1 = new Token({
+  prj: 'XSea',
+  ver: '*',
+  name: 'jimao',
+  type: 'axios',
+  addr: 'http://127.0.0.1:19841',
+}).Sign(priKey);
+const watcher = BigBrother.NewWatcher(token1);
+setInterval(() => {
+  watcher('你好，世界');
+}, 2000);
