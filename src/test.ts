d5194bb8742b88da42640bd61666e9a3f062a996
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-20 13:27:46 +0800

    token生成测试代码

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,17 @@
 import * as BigBrother from './index';
 import fs from 'fs';
+import { Token } from './token';
 
 const priKey = fs.readFileSync('./keys/pri.key', 'utf8');
 const pubKey = fs.readFileSync('./keys/pub.key', 'utf8');
 
 const big = new BigBrother.BigBrother(pubKey);
 big.Start();
+
+// console.log((new Token({
+//   prj: 'XOcean',
+//   ver: '*',
+//   name: 'trump',
+//   type: 'axios',
+//   addr: 'http://172.16.1.194:19841',
+// })).Sign(priKey));
