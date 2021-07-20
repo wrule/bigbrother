90a2c32575ffdcd9097b55c57f433d264c7e3272
jimao <jimao@jimaodeMacBook-Pro.local>
2021-07-20 17:12:08 +0800

    开始准备引入mishu

--- src/collector.ts
+++ src/collector.ts
@@ -1,5 +1,6 @@
 import { ReportFactory } from './report/reportFactory';
 import { ITokenPayload } from './token';
+import fs from 'fs';
 export class Collector {
   public static Collect(
     watcher: ITokenPayload,
@@ -7,6 +8,7 @@ export class Collector {
   ) {
     const report = ReportFactory.Create(watcher, data);
     console.log(`${report.watcherName}:`, report.prjName, report.httpMethod, report.httpPath);
+    fs.writeFileSync(`./jsons/${report.id}.json`, JSON.stringify(report.JsonObject.httpRspData, null, 2));
     // client.index({
     //   index: 'api',
     //   body: report.JsonObject,
